5d563217dcd5487fab9fee597da1b300b4898477
Bleona Hitaj <bleona.hitaj98@gmail.com>
2026-01-06 21:10:58 +0100

    Update the year

--- script.js
+++ script.js
@@ -2,3 +2,5 @@ document.querySelectorAll(".fade-in").forEach((el, i) => {
   setTimeout(() => el.classList.add("show"), i * 800);
 });
 
+document.getElementById("year").textContent=new Date().getFullYear();
+
